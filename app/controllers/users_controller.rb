28ea36c6a928dc90e3cf298e5c9cdc56b3e25f02
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-28 16:21:05 -0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+	before_action: :logged_in?, only: [:show, :index]
 	def index
 		@users = User.all
 	end
@@ -19,6 +19,25 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		if current_user == @user
+			render :show
+		else
+			redirect_to sign_in_path
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		user_id = params[:id]
+		user = User.find(user_id)
+
+		update_attributes = params.require(:user).permit(:first_name, :last_name)
+		user.update_attributes(update_attributes)
+
+		redirect_to "/users/#{@user.id}"
 	end
 
 	private
