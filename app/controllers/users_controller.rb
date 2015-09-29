9dad4dc3fcaba7cb39e30ee6b7e377875744a0ba
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-28 18:51:53 -0700

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,10 +27,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	 def edit
+    @user = User.find(params[:id])
+  end
+
+	def update
+		user = User.find(params[:id])
+		binding.pry
+		new_attributes = params.require(:user).permit(:first_name, :last_name)
+
+		user.update_attributes(new_attributes)
+
+		redirect_to "/users/#{user.id}"
+	end
+
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:first_name, :last_name, :email)
 	end
 
 end
